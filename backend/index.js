f8ed45488b4ce92e580e78b2f6efdf1de8aa6677
Pratik Dhoke <pratikdhoke13@gmail.com>
2023-06-04 12:07:25 +0530

    [BACKEND] Resolve CORS error

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const app = express();
 const codeRouter = require("./routes/saveCode");
 const getRouter = require("./routes/GET/getRequestExample");
 
+// Add a middleware to include the CORS headers
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use("/code", codeRouter);
 app.use("/getrequest", getRouter);
